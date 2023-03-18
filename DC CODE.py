af0d6326805f3ec686c8b04669708ba5d125312a
ioannacoding <127052077+ioannacoding@users.noreply.github.com>
2023-03-18 11:11:18 +0200

    Add files via upload
    Digital Clock using tkinter

--- DC CODE.py
+++ DC CODE.py
@@ -1,15 +1,18 @@
 from tkinter import*
 from tkinter.ttk import*
 from time import strftime
+#setting up the tkinter window and title
 root=Tk()
 root.title('digital clock')
-
+#This method counttime gives the format of the label and it
+#refreshes the label every one thousand mile-seconds
 def counttime():
     string=strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000,counttime)
-    
+#Here the label is placed inside the window position 
 label=Label(root,font=("ds-digital",70),background="black",foreground="green")
 label.pack(anchor="center")
+#This is the first call of countime method
 counttime()
-mainloop()
+
